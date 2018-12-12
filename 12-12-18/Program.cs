b4288aaaa02784ed5c1eaba93e31704c69b3853e
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2018-12-12 08:39:47 +0100

    creato un programma che converte il numero 225 in binario

--- 12-12-18/Program.cs
+++ 12-12-18/Program.cs
@@ -10,6 +10,19 @@ namespace _12_12_18
     {
         static void Main(string[] args)
         {
+            string risultato = " ";
+            int num = 225;
+
+            while (num > 1)
+            {
+                int resto = num % 2;
+                risultato = Convert.ToString(resto) + risultato;
+                num = num / 2;
+            }
+            risultato = Convert.ToString(num) + risultato;
+            Console.WriteLine($"il numero binario è{risultato}");
+
+            Console.ReadLine();
         }
     }
 }
